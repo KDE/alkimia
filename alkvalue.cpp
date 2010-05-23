8fb31051832e28dc10967a2b884f7d6758ac0519
Thomas Baumgart <thb@net-bembel.de>
2010-05-23 13:15:55 +0000

    First import of alkimia code
    svn path=/trunk/playground/office/alkimia/libalkimia/; revision=1129706

---
+++ alkvalue.cpp
@@ -0,0 +1,127 @@
+/***************************************************************************
+ *   Copyright 2010  Thomas Baumgart  ipwizard@users.sourceforge.net       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version.       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
+ ***************************************************************************/
+
+#include "alkvalue.h"
+
+#include <iostream>
+#include <QRegExp>
+
+AlkValue::AlkValue(const QString& str, const QChar& decimalSymbol) :
+  m_val(0)
+{
+  // empty strings are easy
+  if(str.isEmpty())
+    return;
+
+  // take care of mixed prices of the form "5 8/16" as well
+  // as own internal string representation
+  QRegExp regExp("^(?:(\\d+)\\s+|-)?(\\d+)/(\\d+)");
+  //                  +-#1-+        +-#2-+ +-#3-+
+  if (regExp.indexIn(str) > -1) {
+    m_val = qPrintable(str.mid(regExp.pos(2)));
+    m_val.canonicalize();
+    const QString& part1 = regExp.cap(1);
+    if(!part1.isEmpty()) {
+      if(part1 == QLatin1String("-")) {
+        mpq_neg(m_val.get_mpq_t(), m_val.get_mpq_t());
+
+      } else {
+        mpq_class summand(qPrintable(regExp.cap(2)));
+        mpq_add(m_val.get_mpq_t(), m_val.get_mpq_t(), summand.get_mpq_t());
+        m_val.canonicalize();
+      }
+    }
+    return;
+  }
+
+  // qDebug("we got '%s' to convert", qPrintable(str));
+  // everything else gets down here
+  const QString validChars = QString("\\d%1-()").arg(decimalSymbol);
+  const QString negChars = QString("-()");
+  QRegExp invCharSet(QString("[^%1]").arg(validChars));
+  QRegExp negCharSet(QString("[%1]").arg(negChars));
+
+  QString res(str);
+  // get rid of any character that is not allowed.
+  res.remove(invCharSet);
+
+  // qDebug("we reduced it to '%s'", qPrintable(res));
+  // check if number is negative
+  bool isNegative = false;
+  if(res.indexOf(negCharSet) != -1) {
+    isNegative = true;
+    res.remove(negCharSet);
+  }
+
+  // qDebug("and modified it to '%s'", qPrintable(res));
+  // if someone uses the decimal symbol more than once, we get
+  // rid of them except the right most one
+  int pos;
+  while(res.count(decimalSymbol) > 1) {
+    pos = res.indexOf(decimalSymbol);
+    res.remove(pos, 1);
+  }
+
+  // take care of any fraction
+  pos = res.indexOf(decimalSymbol);
+  int len = res.length();
+  QString fraction("/1");
+  if((pos != -1) && (pos < len)) {
+    fraction += QString(len - pos - 1, '0');
+    res.remove(pos, 1);
+  }
+  res += fraction;
+
+  // looks like we now have a pretty normalized string now that we
+  // can convert right away
+  // qDebug("and try to convert '%s'", qPrintable(res));
+  m_val = mpq_class(qPrintable(res));
+  m_val.canonicalize();
+
+  // now we make sure that we use the right sign
+  if(isNegative)
+    m_val = -m_val;
+}
+
+QString AlkValue::toString(void) const
+{
+  char *p = 0;
+  // use the gmp provided conversion routine
+  gmp_asprintf(&p, "%Qd", m_val.get_mpq_t());
+
+  // convert it into a QString
+  QString result(p);
+
+  // and free up the resources allocated by gmp_asprintf
+  __gmp_freefunc_t freefunc;
+  mp_get_memory_functions (NULL, NULL, &freefunc);
+  (*freefunc) (p, std::strlen(p)+1);
+
+  // done
+  return result;
+}
+
+#if 0
+std::ostream& operator<<(std::ostream& ostr, const AlkValue& val)
+{
+  ostr << val.m_val;
+  if(val.m_val.get_den() == 1)
+    ostr << "/1";
+  return ostr;
+}
+#endif
+
---
+++ alkvalue.h
@@ -0,0 +1,282 @@
+/***************************************************************************
+ *   Copyright 2010  Thomas Baumgart  ipwizard@users.sourceforge.net       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version.       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
+ ***************************************************************************/
+
+#ifndef _ALKVALUE_H
+#define _ALKVALUE_H
+
+// So we can save this object
+
+#include <gmpxx.h>
+#include <QString>
+#include <kdemacros.h>
+
+//#include <QDataStream.h>
+
+#define ALKIMIA_EXPORT KDE_EXPORT
+
+/**
+  * This class represents a value within Alkimia
+  *
+  * @author Thomas Baumgart
+  */
+class ALKIMIA_EXPORT AlkValue
+{
+public:
+  // TODO check if we still need those (they are a leftover from the old stuff)
+  enum roundingMethod {
+    RndNever = 0,
+    RndFloor,
+    RndCeil,
+    RndTrunc,
+    RndPromote,
+    RndHalfDown,
+    RndHalfUp,
+    RndRound
+  };
+
+  // Con- / Destructors
+  /**
+    * This is the standard constructor of an AlkValue object.
+    * The value will be initialized to 0.
+    */
+  AlkValue();
+
+  /// The destructor
+  ~AlkValue() {}
+
+  /**
+    * This constructor converts an int into an AlkValue. It can
+    * also convert a rational number when a @a denom is supplied.
+    *
+    * @param num numerator of the rational number
+    * @param denom denominator of the rational number (defaults to 1)
+    */
+  AlkValue( const int num, const unsigned int denom = 1  );
+
+  /**
+    * This constructor converts a QString into an AlkValue.
+    * Several formats are supported:
+    *
+    *  a) prices in the form "8 5/16"
+    *  b) our own 'toString' format
+    *  c) others
+
+    * Others may be enclosed in "(" and ")" and treated as negative.
+    * They may start or end with a dash and treated as negative.
+    * The rightmost non-numeric character is treated as decimal symbol
+    * All other non-numeric characters are skipped
+    */
+  AlkValue(const QString& str, const QChar& decimalSymbol);
+
+  // AlkValue( const double dAmount, const qint64 denom = 100  );
+
+  // assignment operators
+  const AlkValue& operator=(const AlkValue& val);
+  const AlkValue& operator=(int num);
+  const AlkValue& operator=(double num);
+  const AlkValue& operator=(const QString& str);
+
+  // comparison
+  bool operator==( const AlkValue& val ) const;
+  bool operator!=( const AlkValue& val ) const;
+  bool operator<( const AlkValue& val ) const;
+  bool operator>( const AlkValue& val ) const;
+  bool operator<=( const AlkValue& val ) const;
+  bool operator>=( const AlkValue& val ) const;
+
+  // calculation
+  AlkValue operator+( const AlkValue& summand ) const;
+  AlkValue operator-( const AlkValue& minuend ) const;
+  AlkValue operator*( const AlkValue& factor ) const;
+  AlkValue operator/( const AlkValue& divisor ) const;
+
+  // unary operators
+  AlkValue operator-() const;
+  AlkValue& operator+= ( const AlkValue&  val );
+  AlkValue& operator-= ( const AlkValue&  val );
+  AlkValue& operator/= ( const AlkValue&  val );
+  AlkValue& operator*= ( const AlkValue&  val );
+
+  // functions
+
+  /// @return the absolute value of the AlkValue
+  AlkValue abs(void) const;
+
+  /// @return QString representation in form 'num/denom'.
+  QString toString(void) const;
+
+#if 0
+  AlkValue convert(int denom = 100, const roundingMethod how = RndRound) const;
+  double toDouble(void) const;
+
+#endif
+
+
+protected:
+  mpq_class	m_val;
+
+  ALKIMIA_EXPORT friend std::ostream &operator<<(std::ostream &, const AlkValue &);
+  ALKIMIA_EXPORT friend QDataStream &operator<<(QDataStream &, const AlkValue &);
+  ALKIMIA_EXPORT friend QDataStream &operator>>(QDataStream &, AlkValue &);
+
+};
+
+
+
+
+// std::ostream& operator<< (std::ostream&, const AlkValue&);
+
+inline AlkValue::AlkValue() :
+  m_val(0)
+{
+}
+
+inline AlkValue::AlkValue(const int num, const unsigned int denom) :
+  m_val(num, denom)
+{
+}
+
+inline AlkValue AlkValue::operator+(const AlkValue& right) const
+{
+  AlkValue result;
+  mpq_add(result.m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  result.m_val.canonicalize();
+  return result;
+}
+
+inline AlkValue AlkValue::operator-(const AlkValue& right) const
+{
+  AlkValue result;
+  mpq_sub(result.m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  result.m_val.canonicalize();
+  return result;
+}
+
+inline AlkValue AlkValue::operator*(const AlkValue& right) const
+{
+  AlkValue result;
+  mpq_mul(result.m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  result.m_val.canonicalize();
+  return result;
+}
+
+inline AlkValue AlkValue::operator/(const AlkValue& right) const
+{
+  AlkValue result;
+  mpq_div(result.m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  result.m_val.canonicalize();
+  return result;
+}
+
+inline const AlkValue& AlkValue::operator=(const AlkValue& right)
+{
+  m_val = right.m_val;
+  return *this;
+}
+
+inline const AlkValue& AlkValue::operator=(int right)
+{
+  m_val = right;
+  return *this;
+}
+
+inline const AlkValue& AlkValue::operator=(double right)
+{
+  m_val = right;
+  return *this;
+}
+
+inline const AlkValue& AlkValue::operator=(const QString& right)
+{
+  m_val = AlkValue(right, '.').m_val;
+  return *this;
+}
+
+inline AlkValue AlkValue::abs(void) const
+{
+  AlkValue result;
+  mpq_abs(result.m_val.get_mpq_t(), m_val.get_mpq_t());
+  return result;
+}
+
+inline  bool AlkValue::operator==( const AlkValue& val ) const
+{
+  return mpq_equal(m_val.get_mpq_t(), val.m_val.get_mpq_t());
+}
+
+inline  bool AlkValue::operator!=( const AlkValue& val ) const
+{
+  return !mpq_equal(m_val.get_mpq_t(), val.m_val.get_mpq_t());
+}
+
+inline  bool AlkValue::operator<( const AlkValue& val ) const
+{
+  return mpq_cmp(m_val.get_mpq_t(), val.m_val.get_mpq_t()) < 0 ? true : false;
+}
+
+inline  bool AlkValue::operator>( const AlkValue& val ) const
+{
+  return mpq_cmp(m_val.get_mpq_t(), val.m_val.get_mpq_t()) > 0 ? true : false;
+}
+
+inline  bool AlkValue::operator<=( const AlkValue& val ) const
+{
+  return mpq_cmp(m_val.get_mpq_t(), val.m_val.get_mpq_t()) <= 0 ? true : false;
+}
+
+inline  bool AlkValue::operator>=( const AlkValue& val ) const
+{
+  return mpq_cmp(m_val.get_mpq_t(), val.m_val.get_mpq_t()) >= 0 ? true : false;
+}
+
+inline AlkValue AlkValue::operator-() const
+{
+  AlkValue result;
+  mpq_neg(result.m_val.get_mpq_t(), m_val.get_mpq_t());
+  return result;
+}
+
+inline AlkValue& AlkValue::operator+=(const AlkValue& right)
+{
+  mpq_add(m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  m_val.canonicalize();
+  return *this;
+}
+
+inline AlkValue& AlkValue::operator-=(const AlkValue& right)
+{
+  mpq_sub(m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  m_val.canonicalize();
+  return *this;
+}
+
+inline AlkValue& AlkValue::operator*=(const AlkValue& right)
+{
+  mpq_mul(m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  m_val.canonicalize();
+  return *this;
+}
+
+inline AlkValue& AlkValue::operator/=(const AlkValue& right)
+{
+  mpq_div(m_val.get_mpq_t(), m_val.get_mpq_t(), right.m_val.get_mpq_t());
+  m_val.canonicalize();
+  return *this;
+}
+
+#endif
+
