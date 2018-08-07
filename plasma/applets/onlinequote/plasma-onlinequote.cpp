12bdcc984f695908f1bcde6fbf5b4c6ae6b4491e
Ralf Habacker <ralf.habacker@freenet.de>
2018-08-07 11:04:31 +0200

    Add configuration settings to plasma applet

--- plasma/applets/onlinequote/plasma-onlinequote.cpp
+++ plasma/applets/onlinequote/plasma-onlinequote.cpp
@@ -1,23 +1,45 @@
 #include "plasma-onlinequote.h"
 
+#include "alkonlinequote.h"
+#include "alkonlinequotesprofilemanager.h"
+#include "ui_configwidget.h"
+
+#include <QComboBox>
 #include <QPainter>
 #include <QFontMetrics>
 #include <QSizeF>
- 
+#include <QTimer>
+
 #include <plasma/svg.h>
 #include <plasma/theme.h>
- 
+
+#include <KConfigDialog>
+
+
+class MyWidget : public QWidget, public Ui::ConfigWidget
+{
+public:
+    MyWidget()
+    {
+        setupUi((QWidget *)this);
+    }
+};
+
 PlasmaOnlineQuote::PlasmaOnlineQuote(QObject *parent, const QVariantList &args)
     : Plasma::Applet(parent, args),
     m_svg(this),
-    m_icon("document")
+    m_icon("document"),
+    m_widget(0),
+    m_price(0)
 {
+    setHasConfigurationInterface(true);
     m_svg.setImagePath("widgets/background");
     // this will get us the standard applet background, for free!
     setBackgroundHints(DefaultBackground);
     resize(200, 200);
+    slotFetchQuote();
 }
- 
+
 PlasmaOnlineQuote::~PlasmaOnlineQuote()
 {
     if (hasFailedToLaunch()) {
@@ -29,17 +51,90 @@ PlasmaOnlineQuote::~PlasmaOnlineQuote()
 
 void PlasmaOnlineQuote::init()
 {
- 
     // A small demonstration of the setFailedToLaunch function
     if (m_icon.isNull()) {
         setFailedToLaunch(true, "No world to say hello");
     }
-} 
- 
- 
+}
+
+void PlasmaOnlineQuote::configChanged()
+{
+    qDebug() << "configChanged()";
+}
+
+void PlasmaOnlineQuote::createConfigurationInterface(KConfigDialog *parent)
+{
+    AlkOnlineQuotesProfile *p = new AlkOnlineQuotesProfile("kmymoney", AlkOnlineQuotesProfile::Type::KMyMoney);
+    AlkOnlineQuotesProfileManager &manager = AlkOnlineQuotesProfileManager::instance();
+    manager.addProfile(p);
+    AlkOnlineQuoteSource::setProfile(p);
+    m_widget = new MyWidget;
+    QStringList sources = AlkOnlineQuote::quoteSources();
+    m_widget->m_onlineQuote->clear();
+    m_widget->m_onlineQuote->addItems(sources);
+    int index = sources.indexOf(config().readEntry("onlinequote"));
+    m_widget->m_onlineQuote->setCurrentIndex(index);
+    m_widget->m_symbol->setText(config().readEntry("symbol"));
+    m_widget->m_interval->setValue(config().readEntry("interval", 60));
+    KPageWidgetItem *page = parent->addPage(dynamic_cast<QWidget*>(m_widget), "Online Source");
+    connect(parent, SIGNAL(applyClicked()), this, SLOT(slotConfigAccepted()));
+    connect(parent, SIGNAL(okClicked()), this, SLOT(slotConfigAccepted()));
+}
+
+void PlasmaOnlineQuote::slotConfigAccepted()
+{
+    config().writeEntry("onlinequote", m_widget->m_onlineQuote->currentText());
+    config().writeEntry("symbol", m_widget->m_symbol->text());
+    config().writeEntry("interval", m_widget->m_interval->value());
+    config().sync();
+    emit configNeedsSaving();
+    slotFetchQuote();
+    qDebug() << "configAccepted()";
+}
+
+void PlasmaOnlineQuote::slotFetchQuote()
+{
+    if (config().readEntry("symbol").isEmpty() || config().readEntry("interval").toInt() == 0)
+        return;
+    AlkOnlineQuote quote;
+    connect(&quote, SIGNAL(status(QString)), this, SLOT(slotLogStatus(QString)));
+    connect(&quote, SIGNAL(error(QString)), this, SLOT(slotLogError(QString)));
+    connect(&quote, SIGNAL(failed(QString,QString)), this, SLOT(slotLogFailed(QString,QString)));
+    connect(&quote, SIGNAL(quote(QString,QString,QDate,double)), this, SLOT(slotReceivedQuote(QString,QString,QDate,double)));
+    quote.launch(config().readEntry("symbol"), "", config().readEntry("onlinequote"));
+    int interval = config().readEntry("interval").toInt()*1000;
+    qDebug() << "setting timer to " << interval << "ms";
+    QTimer::singleShot(interval, this, SLOT(slotFetchQuote()));
+    // TODO trigger redraw
+}
+
+void PlasmaOnlineQuote::slotLogStatus(const QString &s)
+{
+    qDebug() << s;
+}
+
+void PlasmaOnlineQuote::slotLogError(const QString &s)
+{
+    slotLogStatus(QString("<font color=\"red\"><b>") + s + QString("</b></font>"));
+}
+
+void PlasmaOnlineQuote::slotLogFailed(const QString &id, const QString &symbol)
+{
+    slotLogStatus(QString("%1 %2").arg(id, symbol));
+}
+
+void PlasmaOnlineQuote::slotReceivedQuote(const QString &id, const QString &symbol, const QDate &date, const double &price)
+{
+    Q_UNUSED(id)
+    qDebug() << "got quote" << date << price;
+    m_date = date;
+    m_price = price;
+}
+
 void PlasmaOnlineQuote::paintInterface(QPainter *p,
         const QStyleOptionGraphicsItem *option, const QRect &contentsRect)
 {
+    Q_UNUSED(option)
     p->setRenderHint(QPainter::SmoothPixmapTransform);
     p->setRenderHint(QPainter::Antialiasing);
  
@@ -51,9 +146,20 @@ void PlasmaOnlineQuote::paintInterface(QPainter *p,
     p->drawPixmap(7, 0, m_icon.pixmap((int)contentsRect.width(),(int)contentsRect.width()-14));
     p->save();
     p->setPen(Qt::white);
-    p->drawText(contentsRect,
+    if (true) {
+        qDebug() << "drawing" << m_price;
+        p->drawText(contentsRect,
+                    Qt::AlignVCenter | Qt::AlignHCenter,
+                    config().readEntry("onlinequote"));
+        p->drawText(contentsRect,
+                    Qt::AlignBottom | Qt::AlignHCenter,
+                    config().readEntry("symbol") + " " + QString::number(m_price,8,4));
+
+    } else {
+        p->drawText(contentsRect,
                 Qt::AlignBottom | Qt::AlignHCenter,
                 "Hello Plasmoid!");
+    }
     p->restore();
 }
 
--- plasma/applets/onlinequote/plasma-onlinequote.h
+++ plasma/applets/onlinequote/plasma-onlinequote.h
@@ -7,25 +7,36 @@
 #include <Plasma/Svg>
  
 class QSizeF;
- 
+
+class MyWidget;
+
 // Define our plasma Applet
 class PlasmaOnlineQuote : public Plasma::Applet
 {
     Q_OBJECT
-    public:
-        // Basic Create/Destroy
-        PlasmaOnlineQuote(QObject *parent, const QVariantList &args);
-        ~PlasmaOnlineQuote();
- 
-        // The paintInterface procedure paints the applet to screen
-        void paintInterface(QPainter *p,
-                const QStyleOptionGraphicsItem *option,
-                const QRect& contentsRect);
-	void init();
+public:
+    // Basic Create/Destroy
+    PlasmaOnlineQuote(QObject *parent, const QVariantList &args);
+    ~PlasmaOnlineQuote();
 
-    private:
-        Plasma::Svg m_svg;
-        KIcon m_icon;
+protected slots:
+    void slotConfigAccepted();
+    void slotFetchQuote();
+    void slotReceivedQuote(const QString &id, const QString &symbol, const QDate &data, const double &price);
+    void slotLogStatus(const QString &s);
+    void slotLogError(const QString &s);
+    void slotLogFailed(const QString &id, const QString &symbol);
+protected:
+    // The paintInterface procedure paints the applet to screen
+    void paintInterface(QPainter *p, const QStyleOptionGraphicsItem *option, const QRect& contentsRect);
+    void init();
+    void configChanged();
+    void createConfigurationInterface(KConfigDialog *parent);
+    Plasma::Svg m_svg;
+    KIcon m_icon;
+    MyWidget *m_widget;
+    QDate m_date;
+    double m_price;
 };
 
 #endif
