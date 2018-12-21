b77ee35dbbb914a6f8b969c41575f022bdf75c7a
Thomas Baumgart <thb@net-bembel.de>
2018-12-21 12:36:20 +0100

    Improve testcase output in case of failure

--- autotests/alkdateformattest.cpp
+++ autotests/alkdateformattest.cpp
@@ -29,39 +29,39 @@ void AlkDateFormatTest::testDateFormatKMyMoney()
   try {
     AlkDateFormat format("%mm-%dd-%yyyy");
 
-    QVERIFY(format.convertString("1-5-2005") == QDate(2005, 1, 5));
-    QVERIFY(format.convertString("jan-15-2005") == QDate(2005, 1, 15));
-    QVERIFY(format.convertString("august-25-2005") == QDate(2005, 8, 25));
+    QCOMPARE(format.convertString("1-5-2005"), QDate(2005, 1, 5));
+    QCOMPARE(format.convertString("jan-15-2005"), QDate(2005, 1, 15));
+    QCOMPARE(format.convertString("august-25-2005"), QDate(2005, 8, 25));
 
     format = AlkDateFormat("%mm/%dd/%yy");
 
-    QVERIFY(format.convertString("1/5/05") == QDate(2005, 1, 5));
-    QVERIFY(format.convertString("jan/15/05") == QDate(2005, 1, 15));
-    QVERIFY(format.convertString("august/25/05") == QDate(2005, 8, 25));
+    QCOMPARE(format.convertString("1/5/05"), QDate(2005, 1, 5));
+    QCOMPARE(format.convertString("jan/15/05"), QDate(2005, 1, 15));
+    QCOMPARE(format.convertString("august/25/05"), QDate(2005, 8, 25));
 
     format = AlkDateFormat("%d\\.%m\\.%yy");
 
-    QVERIFY(format.convertString("1.5.05") == QDate(2005, 5, 1));
-    QVERIFY(format.convertString("15.jan.05") == QDate(2005, 1, 15));
-    QVERIFY(format.convertString("25.august.05") == QDate(2005, 8, 25));
+    QCOMPARE(format.convertString("1.5.05"), QDate(2005, 5, 1));
+    QCOMPARE(format.convertString("15.jan.05"), QDate(2005, 1, 15));
+    QCOMPARE(format.convertString("25.august.05"), QDate(2005, 8, 25));
 
     format = AlkDateFormat("%yyyy\\\\%dddd\\\\%mmmmmmmmmmm");
 
-    QVERIFY(format.convertString("2005\\31\\12") == QDate(2005, 12, 31));
-    QVERIFY(format.convertString("2005\\15\\jan") == QDate(2005, 1, 15));
-    QVERIFY(format.convertString("2005\\25\\august") == QDate(2005, 8, 25));
+    QCOMPARE(format.convertString("2005\\31\\12"), QDate(2005, 12, 31));
+    QCOMPARE(format.convertString("2005\\15\\jan"), QDate(2005, 1, 15));
+    QCOMPARE(format.convertString("2005\\25\\august"), QDate(2005, 8, 25));
 
     format = AlkDateFormat("%m %dd, %yyyy");
 
-    QVERIFY(format.convertString("jan 15, 2005") == QDate(2005, 1, 15));
-    QVERIFY(format.convertString("august 25, 2005") == QDate(2005, 8, 25));
-    QVERIFY(format.convertString("january 1st, 2005") == QDate(2005, 1, 1));
+    QCOMPARE(format.convertString("jan 15, 2005"), QDate(2005, 1, 15));
+    QCOMPARE(format.convertString("august 25, 2005"), QDate(2005, 8, 25));
+    QCOMPARE(format.convertString("january 1st, 2005"), QDate(2005, 1, 1));
 
     format = AlkDateFormat("%m %d %y");
 
-    QVERIFY(format.convertString("12/31/50", false, 2000) == QDate(1950, 12, 31));
-    QVERIFY(format.convertString("1/1/90", false, 2000) == QDate(1990, 1, 1));
-    QVERIFY(format.convertString("december 31st, 5", false) == QDate(2005, 12, 31));
+    QCOMPARE(format.convertString("12/31/50", false, 2000), QDate(1950, 12, 31));
+    QCOMPARE(format.convertString("1/1/90", false, 2000), QDate(1990, 1, 1));
+    QCOMPARE(format.convertString("december 31st, 5", false), QDate(2005, 12, 31));
   } catch (const AlkException &e) {
     QFAIL(qPrintable(e.what()));
   }
@@ -76,54 +76,54 @@ void AlkDateFormatTest::testDateFormatSkrooge()
     AlkDateFormat format2("MMM-dd-yyyy");
     AlkDateFormat format3("MMMM-dd-yyyy");
 
-    QVERIFY(format1.convertString("1-5-2005") == QDate(2005, 1, 5));
-    QVERIFY(format2.convertString("Dez.-15-2005") == QDate(2005, 12, 15));
-    QVERIFY(format2.convertString("Dez-15-2005") == QDate(2005, 12, 15));
-    QVERIFY(format2.convertString("Dec-15-2005") == QDate(2005, 12, 15));
-    QVERIFY(format3.convertString("august-25-2005") == QDate(2005, 8, 25));
+    QCOMPARE(format1.convertString("1-5-2005"), QDate(2005, 1, 5));
+    QCOMPARE(format2.convertString("Dez.-15-2005"), QDate(2005, 12, 15));
+    QCOMPARE(format2.convertString("Dez-15-2005"), QDate(2005, 12, 15));
+    QCOMPARE(format2.convertString("Dec-15-2005"), QDate(2005, 12, 15));
+    QCOMPARE(format3.convertString("august-25-2005"), QDate(2005, 8, 25));
 
     format1 = AlkDateFormat("M/d/yy");
     format2 = AlkDateFormat("MMM/dd/yy");
     format3 = AlkDateFormat("MMMM/dd/yy");
 
-    QVERIFY(format1.convertString("1/5/05") == QDate(2005, 1, 5));
-    QVERIFY(format2.convertString("jan/15/05") == QDate(2005, 1, 15));
-    QVERIFY(format3.convertString("august/25/05") == QDate(2005, 8, 25));
+    QCOMPARE(format1.convertString("1/5/05"), QDate(2005, 1, 5));
+    QCOMPARE(format2.convertString("jan/15/05"), QDate(2005, 1, 15));
+    QCOMPARE(format3.convertString("august/25/05"), QDate(2005, 8, 25));
 
     format1 = AlkDateFormat("d.M.yy");
     format2 = AlkDateFormat("dd.MMM.yy");
     format3 = AlkDateFormat("dd.MMMM.yy");
 
-    QVERIFY(format1.convertString("1.5.05") == QDate(2005, 5, 1));
-    QVERIFY(format2.convertString("15.jan.05") == QDate(2005, 1, 15));
-    QVERIFY(format3.convertString("25.august.05") == QDate(2005, 8, 25));
+    QCOMPARE(format1.convertString("1.5.05"), QDate(2005, 5, 1));
+    QCOMPARE(format2.convertString("15.jan.05"), QDate(2005, 1, 15));
+    QCOMPARE(format3.convertString("25.august.05"), QDate(2005, 8, 25));
 
     format1 = AlkDateFormat("yyyy\\dd\\MM");
     format2 = AlkDateFormat("yyyy\\dd\\MMM");
     format3 = AlkDateFormat("yyyy\\dd\\MMMM");
 
-    QVERIFY(format1.convertString("2005\\31\\12") == QDate(2005, 12, 31));
-    QVERIFY(format2.convertString("2005\\15\\jan") == QDate(2005, 1, 15));
-    QVERIFY(format3.convertString("2005\\25\\august") == QDate(2005, 8, 25));
+    QCOMPARE(format1.convertString("2005\\31\\12"), QDate(2005, 12, 31));
+    QCOMPARE(format2.convertString("2005\\15\\jan"), QDate(2005, 1, 15));
+    QCOMPARE(format3.convertString("2005\\25\\august"), QDate(2005, 8, 25));
 
     format1 = AlkDateFormat("MMM dd, yyyy");
     format2 = AlkDateFormat("MMMM dd, yyyy");
     format3 = AlkDateFormat("MMMM ddd, yyyy");
 
-    QVERIFY(format1.convertString("jan 15, 2005") == QDate(2005, 1, 15));
-    QVERIFY(format2.convertString("august 25, 2005") == QDate(2005, 8, 25));
+    QCOMPARE(format1.convertString("jan 15, 2005"), QDate(2005, 1, 15));
+    QCOMPARE(format2.convertString("august 25, 2005"), QDate(2005, 8, 25));
 
     format1 = AlkDateFormat("UNIX");
-    QVERIFY(format1.convertString("1105743600") == QDate(2005, 1, 15));
+    QCOMPARE(format1.convertString("1105743600"), QDate(2005, 1, 15));
 
     // not supported
 #if 0
-    QVERIFY(format3.convertString("january 1st, 2005") == QDate(2005, 1, 1));
+    QCOMPARE(format3.convertString("january 1st, 2005"), QDate(2005, 1, 1));
     format = AlkDateFormat("%m %d %y");
 
-    QVERIFY(format.convertString("12/31/50", false, 2000) == QDate(1950, 12, 31));
-    QVERIFY(format.convertString("1/1/90", false, 2000) == QDate(1990, 1, 1));
-    QVERIFY(format.convertString("december 31st, 5", false) == QDate(2005, 12, 31));
+    QCOMPARE(format.convertString("12/31/50", false, 2000), QDate(1950, 12, 31));
+    QCOMPARE(format.convertString("1/1/90", false, 2000), QDate(1990, 1, 1));
+    QCOMPARE(format.convertString("december 31st, 5", false), QDate(2005, 12, 31));
 #endif
 } catch (const AlkException &e) {
     QFAIL(qPrintable(e.what()));
