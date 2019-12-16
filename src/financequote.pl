d6b7292343c3d6308219827b4e0fbb12d1877ec5
Thomas Baumgart <thb@net-bembel.de>
2019-12-16 18:53:58 +0100

    Fix minor EBN issues
    (cherry picked from commit 6a09bf29caf1522cfd25d82b564ddf84ddea2578)

--- src/financequote.pl
+++ src/financequote.pl
@@ -4,23 +4,23 @@
 ###
 ### derived from GnuCash finance-quote-helper script which is
 ### Copyright 2001 Rob Browning <rlb@cs.utexas.edu>
-### 
-### This program is free software; you can redistribute it and/or    
-### modify it under the terms of the GNU General Public License as   
-### published by the Free Software Foundation; either version 2 of   
-### the License, or (at your option) any later version.              
-###                                                                  
-### This program is distributed in the hope that it will be useful,  
-### but WITHOUT ANY WARRANTY; without even the implied warranty of   
-### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
-### GNU General Public License for more details.                     
-###                                                                  
+###
+### This program is free software; you can redistribute it and/or
+### modify it under the terms of the GNU General Public License as
+### published by the Free Software Foundation; either version 2 of
+### the License, or (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
 ### You should have received a copy of the GNU General Public License
 ### along with this program# if not, contact:
 ###
-### Free Software Foundation           Voice:  +1-617-542-5942
-### 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
-### Boston, MA  02111-1307,  USA       gnu@gnu.org
+### Free Software Foundation, Inc.     Voice:  +1-617-542-5942
+### 51 Franklin Street, Fifth Floor,   Fax:    +1-617-542-2652
+### Boston, MA  02110-1301,  USA       gnu@gnu.org
 ######################################################################
 
 #use diagnostics; # while testing
@@ -85,7 +85,7 @@ if ($listonly) {
 }
 
 my $source = $ARGV[0];
-my $symbol = $ARGV[1]; 
+my $symbol = $ARGV[1];
 
 #print "\tfinding price for <$symbol> from <$source>\n";
 my  %qhash = $q->fetch($source, $symbol); # get price data from F::Q
@@ -106,7 +106,7 @@ if ($errcode != 0) {
     $usdate = $qhash{$symbol, "date"};
     if ($usdate != "") {
         ($month,$day,$year) = ($usdate =~ /([0-9]+)\/([0-9]+)\/([0-9]+)/);
-        # i'm sure I can do the folowing with a regex but I'm just too idle...
+        # i'm sure I can do the following with a regex but I'm just too idle...
         $month = "0$month" if ($month < 9);
         $day = "0$day" if ($day < 9);
         $yyyymmdd = "$year-$month-$day";
