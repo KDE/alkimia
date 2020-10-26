8c3b8add17a0beffc3690f68c096a6e20da42079
Ralf Habacker <ralf.habacker@freenet.de>
2020-10-26 10:06:44 +0100

    Add custom push button class AlkPushButton

---
+++ src/alkpushbutton.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright 2020 Ralf Habacker <ralf.habacker@freenet.de>               *
+ *                                                                         *
+ *   This file is part of libalkimia.                                      *
+ *                                                                         *
+ *   libalkimia is free software; you can redistribute it and/or           *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2.1 of   *
+ *   the License or (at your option) version 3 or any later version.       *
+ *                                                                         *
+ *   libalkimia is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
+ ***************************************************************************/
+
+#ifndef ALKPUSHBUTTON_H
+#define ALKPUSHBUTTON_H
+
+#include <QtGlobal>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <QPushButton>
+#define AlkPushButton QPushButton
+#else
+#include <KPushButton>
+#define AlkPushButton KPushButton
+#endif
+
+#endif
