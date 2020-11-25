136fda94e6627ad19d45e39c7c516b81a2f78609
Ralf Habacker <ralf.habacker@freenet.de>
2020-11-25 20:44:46 +0100

    Add tools/ci-run-docker-image.sh

---
+++ tools/ci-run-docker-image.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# @author Ralf Habacker <ralf.habacker@freenet.de>
+#
+
+# enter alkimia source dir
+# cd ~/src/alkimia
+
+#inside docker run
+cat << EOF
+#inside docker run the following commands
+
+cd /mnt
+
+ci_variant=kf5 tools/ci-install.sh
+ci_variant=kf5 tools/ci-build.sh
+
+# or
+
+ci_variant=kde4 tools/ci-install.sh
+ci_variant=kde4 tools/ci-build.sh
+EOF
+
+sudo docker pull opensuse/leap
+sudo docker run -v $PWD:/mnt -it opensuse/leap /bin/bash
+
