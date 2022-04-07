c65d1980a26b505ee0e35bdc3ebb82967f602f5a
Ralf Habacker <ralf.habacker@freenet.de>
2022-04-07 18:45:37 +0200

    Migrate to SPDX license strings

--- src/alkcompany.h
+++ src/alkcompany.h
@@ -1,21 +1,10 @@
-/***************************************************************************
- *   Copyright 2011  Alvaro Soliverez  asoliverez@kde.org                  *
- *                                                                         *
- *   This file is part of libalkimia.                                      *
- *                                                                         *
- *   libalkimia is free software; you can redistribute it and/or           *
- *   modify it under the terms of the GNU Lesser General Public License    *
- *   as published by the Free Software Foundation; either version 2.1 of   *
- *   the License or (at your option) version 3 or any later version.       *
- *                                                                         *
- *   libalkimia is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
- ***************************************************************************/
+/*
+    SPDX-FileCopyrightText: 2011 Alvaro Soliverez asoliverez @kde.org
+
+    This file is part of libalkimia.
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #ifndef ALKCOMPANY_H
 #define ALKCOMPANY_H
--- src/alkonlinequoteprocess.h
+++ src/alkonlinequoteprocess.h
@@ -1,22 +1,11 @@
-/***************************************************************************
- *   Copyright 2004  Ace Jones <acejones@users.sourceforge.net>            *
- *   Copyright 2004  Thomas Baumgart <thb@net-bembel.de>                   *
- *                                                                         *
- *   This file is part of libalkimia.                                      *
- *                                                                         *
- *   libalkimia is free software; you can redistribute it and/or           *
- *   modify it under the terms of the GNU General Public License           *
- *   as published by the Free Software Foundation; either version 2.1 of   *
- *   the License or (at your option) version 3 or any later version.       *
- *                                                                         *
- *   libalkimia is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
- ***************************************************************************/
+/*
+    SPDX-FileCopyrightText: 2004 Ace Jones acejones @users.sourceforge.net
+    SPDX-FileCopyrightText: 2004 Thomas Baumgart <thb@net-bembel.de>
+
+    This file is part of libalkimia.
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef ALKONLINEQUOTEPROCESS_H
 #define ALKONLINEQUOTEPROCESS_H
@@ -29,7 +18,7 @@
 Helper class to attend the process which is running the script, in the case
 of a local script being used to fetch the quote.
 
-@author Thomas Baumgart <thb@net-bembel.de> & Ace Jones <acejones@users.sourceforge.net>
+@author Thomas Baumgart <thb@net-bembel.de> & Ace Jones acejones @users.sourceforge.net
 */
 class ALK_NO_EXPORT AlkOnlineQuoteProcess : public KProcess
 {
