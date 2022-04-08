ad31a931c1721971af057b346a409406c8c3dcd5
Ralf Habacker <ralf.habacker@freenet.de>
2022-04-08 10:47:38 +0200

    Add appstream data support for onlinequoteseditor
    BUG:451608
    FIXED-IN:8.1.1

---
+++ tools/onlinequoteseditor/org.kde.onlinequoteseditor.appdata.xml.cmake
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<component type="desktop-application">
+  <id>org.kde.onlinequoteseditor@TARGET_SUFFIX@</id>
+  <metadata_license>CC0-1.0</metadata_license>
+  <project_license>GPL-2.0+</project_license>
+  <name>Online Quotes Editor</name>
+  <name xml:lang="ca">Editor de cotitzacions en línia</name>
+  <name xml:lang="ca-valencia">Editor de cotitzacions en línia</name>
+  <name xml:lang="de">Editor für Online-Kursnotizen</name>
+  <name xml:lang="el">Επεξεργαστής προσφορών</name>
+  <name xml:lang="en_GB">Online Quotes Editor</name>
+  <name xml:lang="es">Editor de cotizaciones en línea</name>
+  <name xml:lang="et">Võrgukursside redaktor</name>
+  <name xml:lang="eu">Lerroko kotizazioen editorea</name>
+  <name xml:lang="fr">Éditeur de cotations en ligne</name>
+  <name xml:lang="gl">Editor de cotizacións de Internet</name>
+  <name xml:lang="hu">Szerkesztő online árajánlatokhoz</name>
+  <name xml:lang="ia">Editor de citationes in linea</name>
+  <name xml:lang="it">Editor delle quotazioni in linea</name>
+  <name xml:lang="ko">온라인 시세 편집기</name>
+  <name xml:lang="nl">Bewerker voor Online koersen</name>
+  <name xml:lang="pl">Edytor wycen z sieci</name>
+  <name xml:lang="pt">Editor de Cotações 'Online'</name>
+  <name xml:lang="pt_BR">Editor de cotações online</name>
+  <name xml:lang="ro">Redactor de cotații online</name>
+  <name xml:lang="sk">Editor online ponúk</name>
+  <name xml:lang="sl">Urejevalni spletnih navedb</name>
+  <name xml:lang="sv">Direktkurseditor</name>
+  <name xml:lang="uk">Редактор інтернет-курсів</name>
+  <name xml:lang="zh_CN">在线报价编辑器</name>
+  <summary>Editor for online price quotes used by finance applications</summary>
+  <summary xml:lang="ca">Editor per a la cotització en línia dels preus usat per les aplicacions financeres</summary>
+  <summary xml:lang="ca@valencia">Editor per a la cotització en línia dels preus usat per les aplicacions financeres</summary>
+  <summary xml:lang="de">Editor für Online-Kursnotizen für Finanz-Anwendungen</summary>
+  <summary xml:lang="el">Επεξεργαστής προσφορών τιμών για χρηματοοικονομικές εφαρμογές</summary>
+  <summary xml:lang="en_GB">Editor for online price quotes used by finance applications</summary>
+  <summary xml:lang="es">Editor de cotizaciones de precios en línea usado por aplicaciones de finanzas</summary>
+  <summary xml:lang="et">Finantsrakenduste võrgukursside hinna redaktor</summary>
+  <summary xml:lang="eu">Finantza-aplikazioek lerroko prezioen kotizazioetarako erabilitako editorea</summary>
+  <summary xml:lang="fr">Éditeur de cotations en ligne utilisé par les applications financières</summary>
+  <summary xml:lang="gl">Editor de cotizacións de Internet usado por aplicacións financeiras</summary>
+  <summary xml:lang="hu">Szerkesztő pénzügyi alkalmazások által használt online árajánlatokhoz</summary>
+  <summary xml:lang="ia">Editor per citationes de prcio in linea usate per applicationes de financia</summary>
+  <summary xml:lang="it">Editor per le quotazioni dei prezzi in linea utilizzate dalle applicazioni finanziarie</summary>
+  <summary xml:lang="ko">재무 관리 프로그램에서 사용하는 온라인 가격 시세 편집기</summary>
+  <summary xml:lang="nl">Bewerker voor online koersen gebruikt door financiële toepassingen</summary>
+  <summary xml:lang="pl">Edytor dla wycen z sieci używanych przez aplikacje finansowe</summary>
+  <summary xml:lang="pt">Editor para cotações de preços 'online' usadas pelas aplicações financeiras</summary>
+  <summary xml:lang="pt_BR">Editor de cotações de preço online usado por aplicativos financeiros</summary>
+  <summary xml:lang="ro">Redactor pentru cotații online de prețuri folosite de aplicațiile financiare</summary>
+  <summary xml:lang="sk">Editor pre online cenové ponuky používané finančnými aplikáciami</summary>
+  <summary xml:lang="sl">Urejevalnik spletnih navedb cen pridobljenih od finančnih aplikacij</summary>
+  <summary xml:lang="sv">Editor för direktkurser använda av ekonomiprogram</summary>
+  <summary xml:lang="uk">Редактор інтернет-курсів, який використовується у фінансових програмах</summary>
+  <summary xml:lang="x-test">xxEditor for online price quotes used by finance applicationsxx</summary>
+  <summary xml:lang="zh_CN">金融应用程序使用的在线报价编辑器</summary>
+  <description>
+    <p>Online Quotes Editor is a graphical user interface application for editing and checking online quotes. Available online qoute sources can be added from the KDE store, created locally, edited and checked for correct execution.</p>.
+    <p xml:lang="de">Online Kurs Editor ist eine Anwendung mit grafischer Benutzeroberfläche zur Bearbeitung und Überprüfung von Online-Kursen. Verfügbare Online-Kursquellen können aus dem KDE Warenhaus hinzugefügt, lokal erstellt, bearbeitet und auf korrekte Ausführung geprüft werden.</p>.
+    <p>The interface is divided into customizable sub-windows for profiles, profile details, assigned online quotes, quote attributes, debug, and browser, allowing for flexible interface setup.</p>
+    <p xml:lang="de">Die Oberfläche ist in anpassbare Unterfenster für Profile, Profildetails, zugewiesene Online-Kursangebote, Angebotsattribute, sund Browser unterteilt, was eine flexible Oberflächengestaltung ermöglicht.</p>
+    <p>Multiple applications can be supported via profiles.</p>
+    <p xml:lang="de">Mehrere Anwendungen können über Profile unterstützt werden.</p>
+    <p>Financial applications that do not contain their own user interface for editing online quote sources can call the online quote editor for this purpose.</p>
+    <p xml:lang="de">Finanzanwendungen, die keine eigene Benutzeroberfläche für die Bearbeitung von Online-Kursquellen enthalten, können den Online-Kurseditor zu diesem Zweck aufrufen.</p>
+  </description>
+  <url type="homepage">https://community.kde.org/Alkimia</url>
+  <url type="bugtracker">https://bugs.kde.org/enter_bug.cgi?format=guided%26product=libalkimia%26component=Online+Quotes+Editor</url>
+  <url type="donation">https://www.kde.org/community/donations/?app=onlinequoteseditor</url>
+  <launchable type="desktop-id">org.kde.onlinequoteseditor.desktop</launchable>
+  <screenshots>
+    <screenshot type="default">
+      <image>https://cdn.kde.org/screenshots/onlinequoteseditor/onlinequoteseditor.png</image>
+    </screenshot>
+  </screenshots>
+  <provides>
+    <binary>onlinequoteseditor@TARGET_SUFFIX@</binary>
+  </provides>
+  <project_group>KDE</project_group>
+  <releases>
+  </releases>
+</component>
