a60fc25ada18f321907ad790de2687dd04c59d1f
l10n daemon script <scripty@kde.org>
2022-04-21 02:10:07 +0000

    GIT_SILENT made messages (after extraction)

--- tools/onlinequoteseditor/org.kde.onlinequoteseditor.appdata.xml.cmake
+++ tools/onlinequoteseditor/org.kde.onlinequoteseditor.appdata.xml.cmake
@@ -5,24 +5,23 @@
   <project_license>GPL-2.0+</project_license>
   <name>Online Quotes Editor</name>
   <name xml:lang="ca">Editor de cotitzacions en línia</name>
+  <name xml:lang="ca-valencia">Editor de cotitzacions en línia</name>
   <name xml:lang="ia">Editor de citationes in linea</name>
+  <name xml:lang="it">Editor delle quotazioni in linea</name>
   <name xml:lang="sv">Direktkurseditor</name>
   <name xml:lang="uk">Редактор інтернет-курсів</name>
   <name xml:lang="x-test">xxOnline Quotes Editorxx</name>
   <summary>Editor for online price quotes used by finance applications</summary>
   <summary xml:lang="ca">Editor per a la cotització en línia dels preus usat per les aplicacions financeres</summary>
+  <summary xml:lang="ca-valencia">Editor per a la cotització en línia dels preus usat per les aplicacions financeres</summary>
   <summary xml:lang="ia">Editor per citationes de prcio in linea usate per applicationes de financia</summary>
+  <summary xml:lang="it">Editor delle quotazioni in linea usato delle applicazioni finanziarie</summary>
   <summary xml:lang="sv">Editor för direktkurser använd av ekonomiprogram</summary>
   <summary xml:lang="uk">Редактор для джерел курсів цін у інтернеті, який використовується фінансовими програмами</summary>
   <summary xml:lang="x-test">xxEditor for online price quotes used by finance applicationsxx</summary>
   <description>.
     .
     <p>The interface is divided into customizable sub-windows for profiles, profile details, assigned online quotes, quote attributes, debug, and browser, allowing for flexible interface setup.</p><p>Multiple applications can be supported via profiles.</p><p>Financial applications that do not contain their own user interface for editing online quote sources can call the online quote editor for this purpose.</p></description>
-  <description xml:lang="ca"><p>Poden funcionar aplicacions múltiples a través dels perfils.</p>. . <p>La interfície es divideix en subfinestres personalitzables per a perfils, detalls de perfil, cotitzacions assignades en línia, atributs de les cotitzacions, depuració i navegador, permetent la configuració flexible de la interfície.</p><p>Les aplicacions financeres que no tenen la seva pròpia interfície d'usuari per editar fonts de cotitzacions en línia poden invocar l'editor de cotitzacions en línia per a aquest propòsit.</p></description>
-  <description xml:lang="ia"><_:p-3 xmlns:_="http://itstool.org/extensions/blank/"/>. . <p>Le interfacie esdividite in sub-fenestras personalisabile per profilos, detalios de profilo, citationes assignate in linea, attributos de citationes, cribrar(debug), e navigar,permittente un configuration de interfacie flexibile.  </p><p>Applicationes multiple pote esser supportate via profilos. </p><p>Applicationes finantiari que non contine lor proprie interfacie de usator per modificar fontes de citation in linea pote appellar le editor de citation in linea pro iste proposito.</p></description>
-  <description xml:lang="sv"><p>Flera program kan stödjas via profiler.</p>. . <p>Gränssnittet är uppdelat i anpassningsbara delfönster för profiler, profilinformation, tilldelade direktkurser, kursegenskaper, felsökning, och webbläsare, vilket tillåter flexibel gränssnittsinställning.</p><p>Ekonomiprogram som inte har sitt egna användargränssnitt för att redigera kurskällor på nätet kan anropa direktkurseditorn i detta syfte.</p></description>
-  <description xml:lang="uk">. . <p>Інтерфейс програми поділено на придатні до налаштовування підвікна для профілів, параметрів профілів, призначених інтернет-курсів, атрибутів курсів, діагностики та панель навігації. Усе це уможливлює гнучке налаштовування інтерфейсу.</p><p>Передбачено можливість підтримки декількох програм із різними профілями.</p><p>Фінансові програми, у яких немає власного інтерфейсу для редагування джерел інтернет-курсів, можуть викликати з цією метою «Редактор інтернет-курсів».</p></description>
-  <description xml:lang="x-test">xx<p>Online Quotes Editor is a graphical user interface application for editing and checking online quotes. Available online quote sources can be added from the KDE store, created locally, edited and checked for correct execution.</p>. . <p>xxThe interface is divided into customizable sub-windows for profiles, profile details, assigned online quotes, quote attributes, debug, and browser, allowing for flexible interface setup.xx</p><p>xxMultiple applications can be supported via profiles.xx</p><p>xxFinancial applications that do not contain their own user interface for editing online quote sources can call the online quote editor for this purpose.xx</p>xx</description>
   <url type="homepage">https://community.kde.org/Alkimia</url>
   <url type="bugtracker">https://bugs.kde.org/enter_bug.cgi?format=guided%26product=libalkimia%26component=Online+Quotes+Editor</url>
   <url type="donation">https://www.kde.org/community/donations/?app=onlinequoteseditor</url>
