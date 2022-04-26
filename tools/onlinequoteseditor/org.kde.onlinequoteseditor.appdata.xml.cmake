695903d0a17cbe108eeee57833eb4a8c35a6596b
l10n daemon script <scripty@kde.org>
2022-04-26 02:08:53 +0000

    GIT_SILENT made messages (after extraction)

--- tools/onlinequoteseditor/org.kde.onlinequoteseditor.appdata.xml.cmake
+++ tools/onlinequoteseditor/org.kde.onlinequoteseditor.appdata.xml.cmake
@@ -12,6 +12,7 @@
   <name xml:lang="ia">Editor de citationes in linea</name>
   <name xml:lang="it">Editor delle quotazioni in linea</name>
   <name xml:lang="nl">Bewerker voor Online koersen</name>
+  <name xml:lang="pl">Edytor wycen z sieci</name>
   <name xml:lang="sk">Editor online ponúk</name>
   <name xml:lang="sl">Urejevalnik spletnih kotacij</name>
   <name xml:lang="sv">Direktkurseditor</name>
@@ -26,21 +27,15 @@
   <summary xml:lang="ia">Editor per citationes de prcio in linea usate per applicationes de financia</summary>
   <summary xml:lang="it">Editor delle quotazioni in linea usato delle applicazioni finanziarie</summary>
   <summary xml:lang="nl">Bewerker voor online koersen gebruikt door financiële toepassingen</summary>
+  <summary xml:lang="pl">Edytor dla wycen z sieci używanych przez aplikacje finansowe</summary>
   <summary xml:lang="sk">Editor pre online cenové ponuky používané finančnými aplikáciami</summary>
   <summary xml:lang="sl">Urednik spletnih kotacij, ki jih uporabljajo finančne aplikacije</summary>
   <summary xml:lang="sv">Editor för direktkurser använd av ekonomiprogram</summary>
   <summary xml:lang="uk">Редактор для джерел курсів цін у інтернеті, який використовується фінансовими програмами</summary>
   <summary xml:lang="x-test">xxEditor for online price quotes used by finance applicationsxx</summary>
-  <description>
-    <p>Online Quotes Editor is a graphical user interface application for editing and checking online quotes. Available online quote sources can be added from the KDE store, created locally, edited and checked for correct execution.</p>.
-    <p xml:lang="de">Online Kurs Editor ist eine Anwendung mit grafischer Benutzeroberfläche zur Bearbeitung und Überprüfung von Online-Kursen. Verfügbare Online-Kursquellen können aus dem KDE Warenhaus hinzugefügt, lokal erstellt, bearbeitet und auf korrekte Ausführung geprüft werden.</p>.
-    <p>The interface is divided into customizable sub-windows for profiles, profile details, assigned online quotes, quote attributes, debug, and browser, allowing for flexible interface setup.</p>
-    <p xml:lang="de">Die Oberfläche ist in anpassbare Unterfenster für Profile, Profildetails, zugewiesene Online-Kursangebote, Angebotsattribute, sund Browser unterteilt, was eine flexible Oberflächengestaltung ermöglicht.</p>
-    <p>Multiple applications can be supported via profiles.</p>
-    <p xml:lang="de">Mehrere Anwendungen können über Profile unterstützt werden.</p>
-    <p>Financial applications that do not contain their own user interface for editing online quote sources can call the online quote editor for this purpose.</p>
-    <p xml:lang="de">Finanzanwendungen, die keine eigene Benutzeroberfläche für die Bearbeitung von Online-Kursquellen enthalten, können den Online-Kurseditor zu diesem Zweck aufrufen.</p>
-   </description>
+  <description><p>Online Quotes Editor is a graphical user interface application for editing and checking online quotes. Available online quote sources can be added from the KDE store, created locally, edited and checked for correct execution.</p>.
+    .
+    <p>The interface is divided into customizable sub-windows for profiles, profile details, assigned online quotes, quote attributes, debug, and browser, allowing for flexible interface setup.</p><p>Multiple applications can be supported via profiles.</p><p>Financial applications that do not contain their own user interface for editing online quote sources can call the online quote editor for this purpose.</p></description>
   <url type="homepage">https://community.kde.org/Alkimia</url>
   <url type="bugtracker">https://bugs.kde.org/enter_bug.cgi?format=guided%26product=libalkimia%26component=Online+Quotes+Editor</url>
   <url type="donation">https://www.kde.org/community/donations/?app=onlinequoteseditor</url>
