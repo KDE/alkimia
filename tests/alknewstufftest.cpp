29a8ffbf4788770e06f2a94554df60d17d4f6496
Ralf Habacker <ralf.habacker@freenet.de>
2024-01-13 10:39:41 +0100

    Complete alknewstufftest
    This test was added to determine if KF Newstuff support
    is broken due to network problems or other issues.

--- tests/alknewstufftest.cpp
+++ tests/alknewstufftest.cpp
@@ -22,10 +22,54 @@
 #endif
 
 #include <QApplication>
+#include <QPointer>
+
+#include <KAboutData>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    #include <QApplication>
+    #include <KLocalizedString>
+
+    #define _i18n i18n
+    #define LICENCE_GPL KAboutLicense::GPL
+    #define CATALOG
+    #define aboutName() about.displayName()
+#else
+    #include <KApplication>
+    #include <KCmdLineArgs>
+    #include <kmenu.h>
+
+    #undef QStringLiteral
+    #define QStringLiteral QByteArray
+    #define _i18n ki18n
+    #define LICENCE_GPL KAboutData::License_GPL
+    #define CATALOG QByteArray("onlinequoteseditor"),
+    #define aboutName() about.programName()
+#endif
+
+#if defined(Q_OS_WIN) && !defined(BUILD_WITH_KIO)
+#include <QNetworkProxyFactory>
+#endif
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    KAboutData about(QStringLiteral("onlinequoteseditor"),
+                     CATALOG
+                     _i18n("Online Quotes Editor"),
+                     QStringLiteral("1.0"),
+                     _i18n("Editor for online price quotes used by finance applications"),
+                     LICENCE_GPL,
+                     _i18n("(C) 2018-2023 Ralf Habacker"));
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    QApplication app(argc,argv);
+#else
+    KCmdLineArgs::init(argc, argv, &about);
+    KApplication app(true);
+#endif
+
+#if defined(Q_OS_WIN) && !defined(BUILD_WITH_KIO)
+    QNetworkProxyFactory::setUseSystemConfiguration(true);
+#endif
 
     AlkOnlineQuotesProfile profile("test", AlkOnlineQuotesProfile::Type::Alkimia5, "alkimia-quotes.knsrc");
     AlkOnlineQuotesProfileManager::instance().addProfile(&profile);
@@ -33,9 +77,10 @@ int main(int argc, char *argv[])
     QString configFile = profile.hotNewStuffConfigFile();
 
 #if KNEWSTUFF_VERSION < QT_VERSION_CHECK(5, 78, 0)
-    QPointer<KNS3::DownloadDialog> dialog = new KNS3::DownloadDialog(configFile, this);
+    QPointer<KNS3::DownloadDialog> dialog = new KNS3::DownloadDialog(configFile);
     dialog->exec();
     delete dialog;
+    return 0;
 #elif KNEWSTUFF_VERSION < QT_VERSION_CHECK(5, 94, 0)
     KNS3::QtQuickDialogWrapper(configFile).exec();
 #else
