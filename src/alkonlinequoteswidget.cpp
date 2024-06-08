5cc8115bf5adfb040181ee058e6c507e964d964f
Ralf Habacker <ralf.habacker@freenet.de>
2024-06-08 17:16:30 +0200

    In class AlkOnlineQuotesWidget disable delete button for default online quote sources
    They cannot be deleted.

--- src/alkonlinequoteswidget.cpp
+++ src/alkonlinequoteswidget.cpp
@@ -481,11 +481,11 @@ void AlkOnlineQuotesWidget::Private::updateButtonState()
     bool isFinanceQuote = m_currentItem.isFinanceQuote() || m_profile->type() == AlkOnlineQuotesProfile::Type::Script;
     bool hasWriteSupport = (m_profile->type() != AlkOnlineQuotesProfile::Type::None && !isFinanceQuote) || m_profile->hasGHNSSupport();
     bool isRemoteUnpublished = m_currentItem.isGHNS() && m_currentItem.profile()->GHNSFilePath(m_currentItem.name()).isEmpty();
+    bool isDefaultSource = m_profile->defaultQuoteSources().contains(m_currentItem.name());
     m_newButton->setEnabled(hasWriteSupport);
     m_cancelButton->setEnabled(modified);
     m_duplicateButton->setEnabled(hasWriteSupport);
-    // TODO default quote sources are not deletable
-    m_deleteButton->setEnabled((!m_currentItem.isReadOnly() && !m_currentItem.isGHNS()) || isRemoteUnpublished);
+    m_deleteButton->setEnabled((!m_currentItem.isReadOnly() && !m_currentItem.isGHNS() && !isDefaultSource) || isRemoteUnpublished);
     m_uploadButton->setEnabled(m_profile->hasGHNSSupport() && m_currentItem.isGHNS() && AlkOnlineQuoteUploadDialog::isSupported());
     m_acceptButton->setEnabled(modified);
     m_checkButton->setEnabled(isFinanceQuote || !modified);
